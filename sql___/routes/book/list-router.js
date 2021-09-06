f0c4929e2473c6cb32d6d6c14a0ad85171969907
dlstn4509 <dlstn7609@hanmail.net>
2021-09-06 17:57:39 +0900

    수업끝

--- sql___/routes/book/list-router.js
+++ sql___/routes/book/list-router.js
@@ -4,19 +4,19 @@ const express = require('express')
 const router = express.Router()
 const {error, cutTail, chgStatus} = require('../../modules/util')
 const {pool} = require('../../modules/mysql-init')
-const pager = require('../../modules/pager-init')
+const createPager = require('../../modules/pager-init')
 
 router.get(['/', '/:page'], async (req, res, next) => {
   try {
-    const sql = "SELECT COUNT(idx) FROM books"
-    const [[rs]] = await pool.execute(sql)
-    const totalRecord = rs['COUNT(idx)']
+    let sql = "SELECT COUNT(idx) FROM books"
+    const [[cnt]] = await pool.execute(sql)
+    const totalRecord = cnt['COUNT(idx)']
     const page = req.params.page || 1
-    const {listCnt, pagerCnt} = pager(page, totalRecord)
-    res.json({page, listCnt, pagerCnt, totalRecord})
-    /*
-    const sql = 'SELECT * FROM books ORDER BY idx DESC LIMIT ?, ?'
-    const [rs] = await pool.execute(sql) // = [[a], [b]]  
+    const pager = createPager(page, totalRecord, 2, 3)
+    
+    sql = 'SELECT * FROM books ORDER BY idx DESC LIMIT ?, ?'
+    const values = [pager.startIdx.toString(), pager.listCnt.toString()]
+    const [rs] = await pool.execute(sql, values) 
     
     const books = rs.map(v => {
       v.createdAt = moment(v.createdAt).format('YYYY-MM-DD')
@@ -31,9 +31,9 @@ router.get(['/', '/:page'], async (req, res, next) => {
     const js =  'book/list'
     const css =  'book/list'
     
-    res.status(200).render('book/list', {title, description, js, css, books})
+    res.status(200).render('book/list', {title, description, js, css, books, pager})
     // res.status(200).json(rs)
-    */
+   
   }
   catch(err) {
     next(error(err))
