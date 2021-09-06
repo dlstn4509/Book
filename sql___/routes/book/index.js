0155926ca110833b9a674c4964e237b95b32165d
dlstn4509 <dlstn7609@hanmail.net>
2021-09-06 12:31:16 +0900

    router 개선

--- sql___/routes/book/index.js
+++ sql___/routes/book/index.js
@@ -5,10 +5,22 @@ const formRouter = require('./form-router')
 const saveRouter = require('./save-router')
 const listRouter = require('./list-router')
 const viewRouter = require('./view-router')
+const deleteRouter = require('./delete-router')
 
-router.use('/form', formRouter) // HTML: 글작성 페이지
-router.use('/save', saveRouter) // POST: 저장
-router.use('/list', listRouter) // HTML/GET: 리스트
-router.use('/view', viewRouter) // HTML/GET: 상세페이지
+router.post('/', saveRouter)         // POST: 저장
+router.delete('/', viewRouter)       // DELETE: 삭제
+router.use('/form', formRouter)      // HTML: 글작성 페이지
+router.get('/', listRouter)          // HTML/GET: 리스트
+router.get('/:idx', viewRouter)      // HTML/GET: 상세페이지
+
 
 module.exports = router
+
+/* 
+GET        /book            전체리스트 LIST
+GET        /book?page=1     페이지리스트 LIST
+GET        /book/1          상세리스트 LIST
+POST       /book            저장 save
+PUT        /book            업데이트 update
+DELETE     /book            삭제 delete
+*/
