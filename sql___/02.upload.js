882b3866618c2f0ceef684ba280357cd873e4da6
dlstn4509 <dlstn7609@hanmail.net>
2021-09-07 16:18:21 +0900

    multer error 완

--- sql___/02.upload.js
+++ sql___/02.upload.js
@@ -3,6 +3,11 @@ const app = express()
 const path = require('path')
 const uploader = require('./middlewares/multer-mw')
 
+/****************************** view engine *******************/
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.locals.pretty = true
+
 
 require('dotenv').config()
 require('./modules/server-init')(app, process.env.PORT)
@@ -18,3 +23,11 @@ app.post('/file', uploader.single('upfile'), (req, res, next) => {
 
 
 
+
+
+
+/***************************** error init ********************/
+const _404Router = require('./routes/error/404-router')
+const _500Router = require('./routes/error/500-router')
+app.use(_404Router)
+app.use(_500Router)
--- sql___/middlewares/multer-mw.js
+++ sql___/middlewares/multer-mw.js
@@ -2,9 +2,10 @@
 const multer = require('multer')
 const moment = require('moment')
 const path = require('path')
-const fs = require('fs-extra') // 파일 시스템
-const {v4 : uuid} = require('uuid')
-const { uniqueId } = require('lodash')
+const fs = require('fs-extra')
+const { v4: uuid } = require('uuid')
+const { exts } = require('../modules/util')
+const allowExt = [...exts.imgExt, ...exts.docExt, ...exts.mediaExt, ...exts.zipExt]
 const mega = 1024000
 
 const destination = async (req, file, cb) => {  // 폴더명
@@ -16,17 +17,35 @@ const destination = async (req, file, cb) => {  // 폴더명
 	catch(err) {
 		cb(err)
 	}
-}
+}	
 
-const filename = async (req, file, cb) => {  // 파일 이름
+const filename = (req, file, cb) => {  // 파일 이름
+	try {
 		const ext = path.extname(file.originalname).toLowerCase()     // 확장자 (.jpg)
 		// const ext = file.originalname.split('.').pop()   // 확장자 (jpg)
 		const filename = moment().format('YYMMDD') + '_' + uuid() + ext // getTime + 확장자
 		cb(null, filename)
+	}
+	catch(err) {
+		cb(err)
+	}
 }
 
-const storage = multer.diskStorage({destination,filename}) // 폴더 + 파일
+const fileFilter = (req, file, cb) => { // 파일 점검
+	try {
+		const ext = path.extname(file.originalname).substr(1).toLowerCase()
+		if(allowExt.includes(ext)) cb(null, true)
+		else cb(new Error(`첨부하신 파일은 업로드가 허용되지 않습니다 - ${ext}파일`))
+	}
+	catch(err) {
+		cb(err)
+	}
+}
 
+
+const storage = multer.diskStorage({destination,filename}) // 폴더 + 파일
 const limits = {fileSSize : mega * 5} // 용량 제한
 
-module.exports = multer({ storage, limits }) // 최종 업로드
+module.exports = multer({ storage, limits, fileFilter })
+
+
