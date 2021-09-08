a480733e329572eb29234a976645ccc2f8bec6f0
dlstn4509 <dlstn7609@hanmail.net>
2021-09-08 11:18:23 +0900

    첨부파일 완

--- sql___/routes/book/list-router.js
+++ sql___/routes/book/list-router.js
@@ -2,7 +2,7 @@ const path = require('path')
 const moment = require('moment')
 const express = require('express')
 const router = express.Router()
-const {error, cutTail, chgStatus} = require('../../modules/util')
+const {error, cutTail, chgStatus, getIcon} = require('../../modules/util')
 const {pool} = require('../../modules/mysql-init')
 const createPager = require('../../modules/pager-init')
 
@@ -16,16 +16,20 @@ router.get(['/', '/:page'], async (req, res, next) => {
     const page = Number(req.params.page || 1)
     const pager = createPager(page, totalRecord, 5, 3)
     
-    sql = "SELECT * FROM books WHERE status < 3 ORDER BY idx DESC LIMIT ?, ?"
+    sql = `SELECT B.*, F.oriname, F.savename
+          FROM books B LEFT JOIN files F
+          ON b.idx = f.fidx
+          WHERE B.status < 3
+          ORDER BY B.idx DESC LIMIT ?, ?`
     values = [pager.startIdx.toString(), pager.listCnt.toString()]
-    const [rs] = await pool.execute(sql, values)
+    const [books] = await pool.execute(sql, values)
     
-    const books = rs.map(v => {
+    books.forEach(v => {
       v.createdAt = moment(v.createdAt).format('YYYY-MM-DD')
       v.content = cutTail(v.content)
       v.writer = v.writer || '미상'
       v.status = chgStatus(v.status)
-      return v;
+      v.icon = v.savename ? getIcon(v.savename) : null
     })    
 
     const title =  '도서 목록'
