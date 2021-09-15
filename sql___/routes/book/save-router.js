9af16fa4f9c9159db8808b9bea7a9aebfb14aec1
dlstn4509 <dlstn7609@hanmail.net>
2021-09-15 13:07:33 +0900

    업데이트 분기처리 완료

--- sql___/routes/book/save-router.js
+++ sql___/routes/book/save-router.js
@@ -1,3 +1,14 @@
+/* 
+    1. 위의 Query가 INSERT라면 rs = {insertId: 15, affectedRows: 1...}
+    2. 위의 Query가 UPDATE라면 rs = {affectedRows: 1...}
+    3. files Query는 아래와 같다
+      1)신규글이라면 아래와 같이 INSERT만 하면 된다
+        INSERT INTO files SET oriname=?, savename=?, mimetype=?, size=?, fieldname=?, fidx=?  (fidx는 rs.insertId)
+      2)UPDATE라면 일단 k.substr(0, 1).toUpperCase() ? 'C' : 'U' 분기하고, 기존레코드가 있는지 체크한 후 있다면 기존 레코드의 status='0' and 파일도 옮긴다.
+        그리고 1)의 INSERT를 실행한다.
+        UPDATE files SET status='0' WHERE idx=?  (idx는 SELECT * FROM files WHERE fidx = idx)
+*/
+
 const express = require('express')
 const router = express.Router()
 const {error} = require('../../modules/util')
@@ -15,17 +26,26 @@ router.post('/', uploader.fields([{name: 'cover'}, {name: 'upfile'}]), async (re
 		values = [title, writer, content]
 		const [rs] = await pool.execute(sql, values)
 
-
-
-
-/* 
-    1. 위의 Query가 INSERT라면 rs = {insertId: 15, affectedRows: 1...}
-    2. 위의 Query가 UPDATE라면 rs = {affectedRows: 1...}
-    3. files Query는 아래와 같다
-      INSERT INTO files SET oriname=?, savename=?, mimetype=?, size=?, fieldname=?,     fidx=?  (fidx는 rs.insertId)
-      UPDATE files      SET oriname=?, savename=?, mimetype=?, size=?, fieldname=?,     status=? WHERE idx=?  (idx는 SELECT * FROM files WHERE fidx = idx)
-*/
-
+    if(req.files) {
+      let fieldname;
+      for(let [k, [v]] of Object.entries(req.files)) {
+        fieldname = k.substr(0, 1).toUpperCase() // C or U
+        if(isUpdate) {
+          sql = " SELECT idx, savename FROM files WHERE fidx=? AND fieldname=? AND status=? "
+          values = [idx, fieldname, '1']
+          let [rsf] = await pool.execute(sql, values)
+          if(rsf.length > 0) {
+            sql = " UPDATE files SET status = '0' WHERE idx= " + rsf[0].idx
+            await pool.execute(sql)
+            await moveFile(rsf[0].savename)
+          }
+        }
+        sql = " INSERT INTO files oriname=?, savename=?, mimetype=?, size=?, fieldname=?, fidx=? "
+        values = [v.originalname, v.filename, v.mimetype, v.size, v.fieldname, (isUpdate ? idx : rs.insertId)]
+        await pool.execute(sql, values)
+      }
+      res.redirect(`/${req.lang}/book`)
+    }
 
 /* 
     if(req.files) { // 첨부파일이 존재 한다면
@@ -34,17 +54,12 @@ router.post('/', uploader.fields([{name: 'cover'}, {name: 'upfile'}]), async (re
         sql = (_method === 'PUT' && idx) ? " UPDATE files " : " INSERT INTO files "
         sql += " SET oriname=?, savename=?, mimetype=?, size=?, fieldname=? "
 
-
         values = [rs.insertId, originalname, filename, mimetype, size, k.substr(0, 1).toUpperCase()]
         await pool.execute(sql, values)
       }
     } */
 
-
-
-
-
-    res.redirect(`/${req.lang}/book`)
+    // res.redirect(`/${req.lang}/book`)
   }
   catch(err) {
     next(error(500, err))
