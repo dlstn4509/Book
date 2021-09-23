da31d2f8890af9c87abd049168ef25dff5df5087
dlstn4509 <dlstn7609@hanmail.net>
2021-09-23 16:32:19 +0900

    회원가입 검증 완료

--- sql___/public/js/auth/form.js
+++ sql___/public/js/auth/form.js
@@ -62,49 +62,76 @@ function verifyUserid() {
 function verifyPasswd() {
 	var passwd = passwdEl.value.trim()
 	verifyReset(passwdEl, passwdTxt)
-	if(passwd === '') {
+	if(passwd === '' || passwd.length < 6 || passwd.length > 24) {
+		verifyFalse(passwdEl, passwdTxt, passwd === '' ? ERR.PW_NULL : ERR.PW_LEN)
 		return false
 	}
-	if(passwd.length < 6 || passwd.length > 24) {
-		return false
+	else {
+		verifyTrue(passwdEl, passwdTxt)
+		return true
 	}
-	return true
 }
 
 function verifyPasswd2() {
 	var passwd2 = passwd2El.value.trim()
-	if(passwd2 === '') {
+	verifyReset(passwd2El, passwd2Txt)
+	if(passwd2 === '' || passwd2.length < 6 || passwd2.length > 24) {
+		verifyFalse(passwd2El, passwd2Txt, passwd2 === '' ? ERR.PW2_NULL : ERR.PW2_LEN)
 		return false
 	}
-	if(passwd2.length < 6 || passwd2.length > 24) {
-		return false
+	else {
+		verifyTrue(passwd2El, passwd2Txt)
+		return true
 	}
-	return true
 }
 
 function verifyPasswdEqual() {
 	var passwd = passwdEl.value.trim()
-	passwd2 = passwd2El.value.trim()
+	var passwd2 = passwd2El.value.trim()
 	if(passwd !== passwd2) {
+		passwdEl.classList.add('error')
+		passwd2El.classList.add('error')
+		passwdTxt.classList.add('error')
+		passwd2Txt.classList.add('error')
+		passwdTxt.innerHTML = ERR.PW_TAKEN
+		passwd2Txt.innerHTML = ERR.PW_TAKEN
 		return false
 	}
-	return true
+	else {
+		passwdEl.classList.remove('error')
+		passwd2El.classList.remove('error')
+		passwdTxt.classList.remove('error')
+		passwd2Txt.classList.remove('error')
+		passwdTxt.innerHTML = ''
+		passwd2Txt.innerHTML = ''
+		return true
+	}
 }
 
 function verifyUsername() {
 	var username = usernameEl.value.trim()
+	verifyReset(usernameEl, usernameTxt)
 	if(username === '') {
+		verifyFalse(usernameEl, usernameTxt, ERR.NAME_NULL)
 		return false
 	}
-	return true
+	else {
+		verifyTrue(usernameEl, usernameTxt)
+		return true
+	}
 }
 
 function verifyEmail() {
 	var email = emailEl.value.trim()
+	verifyReset(emailEl, emailTxt)
 	if(email === '') {
+		verifyFalse(emailEl, emailTxt, ERR.EMAIL_NULL)
 		return false
 	}
-	return true
+	else {
+		verifyTrue(emailEl, emailTxt)
+		return true
+	}
 }
 
 function verifyReset(el, elTxt) {
@@ -118,12 +145,11 @@ function verifyFalse(el, elTxt, msg) {
 	elTxt.classList.add('error')
 	elTxt.innerHTML = msg
 	el.classList.add('error')
-	el.focus()
 }
 
 function verifyTrue(el, elTxt, msg) {
 	el.classList.add('active')
-	elTxt.innerHTML = msg
+	elTxt.innerHTML = msg || ''
 }
 
 
