ca4638b058da507d09a091c1fc5e5c91b2b789a3
dlstn4509 <dlstn7609@hanmail.net>
2021-09-24 09:23:11 +0900

    test : 연습 파일 삭제

--- sql___/04.asd.js
+++ sql___/04.asd.js
@@ -1,13 +1,9 @@
 const crypto = require('crypto')
-const salt = 'fdgkdl234kljg'
+const salt = `lv;ckbl;er`
 
-let pass = '123456'
+let pass = `123456`
 let pass512 = crypto.createHash('sha512').update(pass + salt).digest('base64')
 console.log(pass512)
-
-let pass2 = '123456'
-let pass512re = crypto.createHash('sha512').update(pass2+salt).digest('base64')
-console.log(pass512re)
 
 
 const bcrypt = require('bcrypt')
@@ -19,19 +15,10 @@ const comparePass = async (pass, hash) => {
 }
 
 const passVerify = async () => {
-	let pass = '123456'
-	let salt = 'fdgkdl234kljg'
+	let pass = `123456`
+	let salt = `fdlkjcvbmw32`
 	let hash = await bcrypt.hash(pass + salt, 5)
-	console.log(hash)
 	let compare = await bcrypt.compare(pass + salt, hash)
 	console.log(compare)
 }
-// passVerify()
-
-
-
-const cryptoJs = require('crypto-js')
-const encrypt = cryptoJs.AES.encrypt('여름이였다...', salt)
-console.log('encrypt : ' + encrypt)
-const decrypt = cryptoJs.AES.decrypt(encrypt, salt).toString(cryptoJs.enc.Utf8)
-console.log('decrypt : ' + decrypt)
+passVerify()
