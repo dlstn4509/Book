7a317e2af043a09acbc49190246ab81e36df23e0
dlstn4509 <dlstn7609@hanmail.net>
2021-09-24 15:52:16 +0900

    feat : models

---
+++ sql___/models/auth/create-user.js
@@ -0,0 +1,42 @@
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+const { pool } = require('../../modules/mysql-init')
+const { isUser, isEmail } = require('./verify-data')
+
+const isValid = (user) => {
+	let {userid, passwd, passwd2, username} = user // req.body
+	if(!validator.isAlphanumeric(userid) || userid.length < 6 || userid.length > 24) {
+		return {msg: '아이디가 형식에 맞지 않습니다.'}
+	}
+	else if(passwd.length < 6 || passwd.length > 24 || passwd2.length < 6 || passwd2.length > 24 || passwd !== passwd2) {
+		return {msg: '패스워드가 형식에 맞지 않습니다.'}
+	}
+	else if(username.length === 0) {
+		return {msg: '이름이 형식에 맞지 않습니다.'}
+	}
+	else if(!validator.isEmail(email)) {
+		return {msg: '이메일이 형식에 맞지 않습니다.'}
+	}
+	else {
+		return true
+	}
+}
+
+module.exports = async (user) => {
+	let {userid, passwd, passwd2, username, email, sql, hashPasswd} = user // req.body
+	let {BCRYPT_SALT: salt, BCRYPT_ROUND: round} = process.env
+
+  try { // 검증
+    hashPasswd = await bcrypt.hash(passwd + salt, Number(round))
+    if(isValid(user) !== true) return {success: false, msg: isValid(user).msg}
+    if(isUser(userid)) return {success: false, msg: '아이디가 존재합니다.'}
+    if(isEmail(email)) return {success: false, msg: '이메일이 존재합니다.'}
+
+		sql = `INSERT INTO users SET userid=?, passwd=?, username=?, email=?`
+		const [rs] = await pool.execute(sql, [userid, hashPasswd, username, email])
+		return rs.affectedRows === 1 ? {success: true} : {success: false, msg: '저장에 실패하였습니다.'}
+  }
+  catch (err) {
+    return {success: false, err}
+  }
+}
--- sql___/models/auth/index.js
+++ sql___/models/auth/index.js
@@ -1,1 +1,4 @@
 // findUser, verifyData, createUser, updateUser, deleteUser
+const createUser = require('./create-user')
+
+module.exports = {createUser}
