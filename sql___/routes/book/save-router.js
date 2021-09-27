a1345c380eed5840ad2d4f7b3fa761611d31b948
dlstn4509 <dlstn7609@hanmail.net>
2021-09-27 20:35:39 +0900

    save - fidx 등록

--- sql___/routes/book/save-router.js
+++ sql___/routes/book/save-router.js
@@ -15,17 +15,26 @@ const {moveFile} = require('../../modules/util')
 const {pool} = require('../../modules/mysql-init')
 const createError = require('http-errors')
 const uploader = require('../../middlewares/multer-book-mw')
+const { isUser } = require('../../middlewares/auth-mw')
 
-router.post('/', uploader.fields([{name: 'cover'}, {name: 'upfile'}]), async (req, res, next) => {
+router.post('/', isUser, uploader.fields([{name: 'cover'}, {name: 'upfile'}]), async (req, res, next) => {
   let sql, values
 	try {
-		const { title, writer, content, _method, idx } = req.body
+		const {title, writer, content, _method, idx} = req.body
 		const isUpdate = (_method === 'PUT' && idx)
 		sql = isUpdate ? " UPDATE books " : " INSERT INTO books "
-		sql += " SET title=?, writer=?, content=? "
+		sql += " SET fidx=?, title=?, writer=?, content=? "
 		sql += isUpdate ? " WHERE idx= "+idx : ""
-		values = [title, writer, content]
+		values = [req.session.user.idx, title, writer, content]
 		const [rs] = await pool.execute(sql, values)
+
+  /*  const {title, writer, content, _method, idx} = req.body
+		const isUpdate = (_method === 'PUT' && idx)
+		sql = isUpdate ? " UPDATE books " : " INSERT INTO books "
+		sql += " SET fidx=?, title=?, writer=?, content=? "
+		sql += isUpdate ? " WHERE idx = " + idx : ""
+		values = [req.session.user.idx, title, writer, content]
+		const [rs] = await pool.execute(sql, values) */
 
     if(req.files) { // 파일이 있을때
       let fieldname;
