503b1c4f48e4b369257fbc9ef7d45a7df801044c
dlstn4509 <dlstn7609@hanmail.net>
2021-11-21 13:39:37 +0900

    book 정리

--- node-review/app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const app = express()
-
-require('./modules/server-init')(app, process.env.PORT)
-
-const memberRouter = require('./routers/member')
-app.use('/member', memberRouter)
--- node-review/modules/server-init.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app, port) => {
-	app.listen(port, () => {
-		console.log("===================")
-		console.log('Server Running !!!!')
-		console.log('127.0.0.1:'+port)
-		console.log("===================")
-	})
-}
-
