8da4e4d31c73ce8f4f8f53d3a4649de56523abe9
dlstn4509 <dlstn7609@hanmail.net>
2021-11-23 16:48:52 +0900

    utill loc 수정

--- sql___/back/routes/auth/form-router.js
+++ sql___/back/routes/auth/form-router.js
@@ -1,30 +1,29 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const createError = require('http-errors')
-const { alert } = require('../../modules/util')
-const { createUser } = require('../../models/auth')
-const { isUser, isGuest } = require('../../middlewares/auth-mw')
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const createError = require('http-errors');
+const { alert } = require('../../modules/util');
+const { createUser } = require('../../models/auth');
+const { isUser, isGuest } = require('../../middlewares/auth-mw');
 
 router.get('/', isGuest, (req, res, next) => {
-	// join 창 보여주기
-	req.app.locals.PAGE = 'JOIN'
-	req.app.locals.js = 'auth/form'
-	req.app.locals.css = 'auth/form'
-	req.app.locals.info = null
-	res.status(200).render('auth/form')
-})
+  // join 창 보여주기
+  req.app.locals.PAGE = 'JOIN';
+  req.app.locals.js = 'auth/form';
+  req.app.locals.css = 'auth/form';
+  req.app.locals.info = null;
+  res.status(200).render('auth/form');
+});
 
 router.post('/', async (req, res, next) => {
-	// 실제 join 처리
-	try {
-		const r = await createUser(req.body)
-		if(r) res.redirect('/')
-		else res.send(alert(ERROR.SQL_ERROR))
-	}
-	catch(err) {
-		next(createError(err))
-	}
-})
+  // 실제 join 처리
+  try {
+    const r = await createUser(req.body);
+    if (r) res.redirect('/book');
+    else res.send(alert(ERROR.SQL_ERROR, '/book'));
+  } catch (err) {
+    next(createError(err));
+  }
+});
 
-module.exports = router
+module.exports = router;
--- sql___/back/routes/auth/logout-router.js
+++ sql___/back/routes/auth/logout-router.js
@@ -1,14 +1,14 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const { alert } = require('../../modules/util')
-const { pool } = require('../../modules/mysql-init')
-const { isUser, isGuest } = require('../../middlewares/auth-mw')
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { alert } = require('../../modules/util');
+const { pool } = require('../../modules/mysql-init');
+const { isUser, isGuest } = require('../../middlewares/auth-mw');
 
 router.get('/', isUser, (req, res, next) => {
-	req.logout()
-	res.locals.user = null
-	res.send(alert('로그아웃 되었습니다.'))
-})
+  req.logout();
+  res.locals.user = null;
+  res.send(alert('로그아웃 되었습니다.', '/book'));
+});
 
-module.exports = router
+module.exports = router;
